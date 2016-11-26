e0525a5d7146ef8f88e93bd43627206b76639fb2
Paul Swift <paulswift@Pauls-MacBook.local>
2016-11-26 13:32:36 -0500

    upstream merge

--- src-client/scripts/create-lugg.js
+++ src-client/scripts/create-lugg.js
@@ -94,7 +94,7 @@ const CreateLugg = React.createClass({
                      <SimpleMapPage {...CharlestonMap}/>
                   </div>
                </div>
-
+               </div>
             </div>
     );
    }
